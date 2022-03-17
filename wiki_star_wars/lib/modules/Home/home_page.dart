bf1b925e84809d66e7c04457e5ea8f2340b6d5c0
Marianna Leandra <mariannaleandra@gmail.com>
2022-03-17 00:29:28 -0300

    Adjust home

--- wiki_star_wars/lib/modules/Home/home_page.dart
+++ wiki_star_wars/lib/modules/Home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wiki_star_wars/modules/Home/home_controller.dart';
 import 'package:wiki_star_wars/widgets/characters_lane.dart';
-import 'package:wiki_star_wars/widgets/loading.dart';
 import 'package:wiki_star_wars/widgets/search_input.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -13,35 +12,36 @@ class HomePage extends GetView<HomeController> {
     final theme = Theme.of(context);
     final size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: theme.backgroundColor,
-        appBar: AppBar(
-          title: Text(
-            'Wiki Star Wars',
-            style: theme.appBarTheme.titleTextStyle,
-          ),
-          backgroundColor: theme.appBarTheme.backgroundColor,
+      backgroundColor: theme.backgroundColor,
+      appBar: AppBar(
+        title: Text(
+          'Wiki Star Wars',
+          style: theme.appBarTheme.titleTextStyle,
         ),
-        body: Container(
-          width: size.width,
-          height: size.height,
-          margin: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              SearchInput(
-                onSearch: controller.onSearch,
+        backgroundColor: theme.appBarTheme.backgroundColor,
+      ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SearchInput(
+              onSearch: controller.onSearch,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: CharactersLane(
+                onCharacterPress: (character) =>
+                    controller.onPressCharacter(character),
+                pagingController: controller.pagingCharacters,
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: CharactersLane(
-                  onCharacterPress: (character) =>
-                      controller.onPressCharacter(character),
-                  pagingController: controller.pagingCharacters,
-                ),
-              ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
